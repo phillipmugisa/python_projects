f09e3ae0af19bc5625f3260883ec9f78b18612ed
phillipmugisa <phillipmugisa4@gmail.com>
2022-06-08 18:13:42 +0300

    restoring news_scrappers

---
+++ news_scrapper/scrapper/scrapper.py
@@ -0,0 +1,30 @@
+"""
+Author: Phillip Mugisa
+Date: 6/08/2022
+
+This program scraps news from top news platforms, the user is able to view news basing on location,
+category
+
+--requirements
+    - beautifulsoup
+    - requests
+
+-- sources
+    - google news
+    - new york times
+    - yahoo news
+
+-- categories
+    - technology
+    - entertainment
+    - fashion
+    - sports
+    - finance
+
+-- locations
+    - Global
+    - USA
+    - Europe
+    - Africa
+    
+"""
